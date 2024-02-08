a6b4e187466e5d30085de6320e6e47a6785197e9
Dhanush <dhanushdybala10@gmail.com>
2024-02-08 11:48:20 +0530

    [Dhanush] Update : Implementation of delegate using int method

--- Program.cs
+++ Program.cs
@@ -8,11 +8,26 @@ namespace ConsoleApp1
 {
     public delegate int Addnumber(int num1, int num2);
     public delegate string AddString(string str1, string str2);
+
+    class prog
+    {
+        public int AddNumber(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public string AddString(string str2, string str1)
+        {
+            return str1 + str2;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            prog prog = new prog();
+            Addnumber del = new Addnumber(prog.AddNumber);
+            int num = del(3, 4);
+            Console.WriteLine(num);
         }
     }
 }
